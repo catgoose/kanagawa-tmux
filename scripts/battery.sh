f8864f3bfcbb8d26d566975c6be97069a6fa41cf
Joshua Tye <21010072+tyetyetye@users.noreply.github.com>
2022-05-29 14:01:23 -0500

    battery only displays if under 90%

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 if [[ `ls /sys/class/power_supply/BAT* 2> /dev/null | wc -l` != "0" ]]; then
-  echo `acpi | cut -d: -f2- | cut -d, -f2 | tr -d '% '`
+  charge=`acpi | cut -d: -f2- | cut -d, -f2 | tr -d '% '`
+  if [[ $charge > "90" ]]; then
+    echo "0"
+  else
+    echo $charge
+  fi
 else
   echo "0"
 fi

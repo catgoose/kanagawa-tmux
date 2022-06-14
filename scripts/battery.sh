a39dd46e11947ae3d997828f2820d495e01368a2
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2022-06-13 19:35:53 -0500

    always show charge

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -2,11 +2,7 @@
 
 if [[ `ls /sys/class/power_supply/BAT* 2> /dev/null | wc -l` != "0" ]]; then
   charge=`acpi | cut -d: -f2- | cut -d, -f2 | tr -d '% '`
-  if [[ $charge > "90" ]]; then
-    echo "0"
-  else
-    echo $charge
-  fi
+  echo $charge
 else
   echo "0"
 fi
